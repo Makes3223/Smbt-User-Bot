d204af1b4b813d45ca0e8fbad1f0599f7157bb71
stone <esm75322@gmail.com>
2022-12-17 15:48:15 +0200

    add something scripts

--- main.py
+++ main.py
@@ -3,19 +3,34 @@ from core_v1.modul import prefixes, app
 from core_v1.modul import modules_help
 from core_v1 import helpp
 
+#moduls
 from moduls import ttype
 from moduls import say
 from moduls import ping
+from moduls import dice
+from moduls import blacklist
+from moduls import google
+from moduls import like
+from moduls import manul
+from moduls import url
+from moduls import spam
+from moduls import leave_chat
+
 from moduls.animation import manul
 from moduls.animation import like
-from moduls import leave_chat
+
 from moduls.random_animation import football
 from moduls.random_animation import hunter
+
 from moduls.love import night
 from moduls.love import hearts
 from moduls.love import hearet
+from moduls.love import night 
 from moduls.love import haert
+
+from moduls.fliptext import fliptext2
 from moduls.fliptext import fliptext
+
 from moduls.animation import think
 from moduls.animation import stupid
 from moduls.animation import stars
