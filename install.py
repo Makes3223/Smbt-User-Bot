8aa32926df018bdc26dca9134c3977f1ec9fabe7
Makes <Esm75322@gmail.com>
2023-02-27 20:32:35 +0200

    add phone number

--- install.py
+++ install.py
@@ -8,7 +8,8 @@ lang_dict = {
 "text_hash": "Enter your api_hash: ",
 "text_app": "Greetings from @MacsimBlin, now you can have fun!",
 "text_app_second": "I congratulate you on the installation of the bot, to see the commands, write '.help'",
-"text_end": "Great, now go to Favorites and read commands, or write a command .moduls or .m"
+"text_end": "Great, now go to Favorites and read commands, or write a command .moduls or .m",
+"text_phone": "Enter your phone_number: "
 },
 "ua": {
 "text_first": "Ваш api_id та api_hash у https://my.telegram.org/",
@@ -16,7 +17,8 @@ lang_dict = {
 "text_hash": "Введіть api_hash: ",
 "text_app": "Вітання від @MacsimBlin, тепер ви можете веселитися!",
 "text_app_second": "Вітаю з установкою бота, для перегляду команд напишіть '.help'",
-"text_end": "Чудово, тепер перейдіть до вибраного та прочитайте команди або напишіть команду .moduls або .m"
+"text_end": "Чудово, тепер перейдіть до вибраного та прочитайте команди або напишіть команду .moduls або .m",
+"text_phone": "Введіть свій номер телефону: "
 },
 "ru": {
 "text_first": "Ваш api_id и api_hash в https://my.telegram.org/",
@@ -24,7 +26,8 @@ lang_dict = {
 "text_hash": "Введите api_hash: ",
 "text_app": "Приветствие от @MacsimBlin, теперь вы можете веселиться!",
 "text_app_second": "Поздравляю с установкой бота, для просмотра команд напишите '.help'",
-"text_end": "отлично, теперь перейдите к избранному и прочтите команды или напишите команду .moduls или .m"
+"text_end": "отлично, теперь перейдите к избранному и прочтите команды или напишите команду .moduls или .m",
+"text_phone": "Введите номер телефона: "
 }
 }
 
@@ -40,17 +43,19 @@ else:
     text_app = lang_dict[lang]["text_app"]
     text_app_second = lang_dict[lang]["text_app_second"]
     text_end = lang_dict[lang]["text_end"]
+    text_phone = lang_dict[lang]["text_phone"]
 
 print(text_first)
 
-api_id = input(text_api)
-api_hash = input(text_hash)
+apiid = input(text_api)
+apihash = input(text_hash)
+phone = input(text_phone)
 
-if api_id == "" or api_hash == "":
+if apiid == "" or apihash == "":
     print("Telegram API_ID/API_HASH error!")
 else:
     async def main():
-        async with Client("my_account", api_id, api_hash) as app:
+        async with Client("my_account", api_id = apiid, api_hash = apihash, phone_number = phone) as app:
             await app.send_message("me", text_app)
             await asyncio.sleep(0.5)
             await app.send_message("me", text_app_second)
